3558fb95bea4a91279b1b62353dab7f8f6f89655
qamos <60465891+qamos@users.noreply.github.com>
2023-05-02 17:52:17 +0300

    sborka v1

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
+		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+
+import pkg from 'gulp'
+const { gulp, src, dest, parallel, series, watch } = pkg
+
+import browserSync   from 'browser-sync'
+import bssi          from 'browsersync-ssi'
+import ssi           from 'ssi'
+
+import gulpSass      from 'gulp-sass'
+import dartSass      from 'sass'
+import sassglob      from 'gulp-sass-glob'
+const  sass          = gulpSass(dartSass)
+import less          from 'gulp-less'
+import lessglob      from 'gulp-less-glob'
+import styl          from 'gulp-stylus'
+
+import cheerio       from 'gulp-cheerio'
+import replace       from 'gulp-replace'
+
+import stylglob      from 'gulp-noop'
+import postCss       from 'gulp-postcss'
+import cssnano       from 'cssnano'
+import autoprefixer  from 'autoprefixer'
+import imagemin      from 'gulp-imagemin'
+import rename      	 from 'gulp-rename'
+import changed       from 'gulp-changed'
+import concat        from 'gulp-concat'
+import rsync         from 'gulp-rsync'
+import del           from 'del'
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+	})
+}
+
+function scripts() {
+	return src(['app/js/*.js', '!app/js/*.min.js'])
+
+		.pipe(concat('app.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src([`app/styles/${preprocessor}/**/*.*`, `!app/styles/${preprocessor}/_*.*`])
+		.pipe(eval(`${preprocessor}glob`)())
+		.pipe(eval(preprocessor)({ 'include css': true }))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function images() {
+	return src(['app/images/src/**/*'])
+		.pipe(changed('app/images/dist'))
+		.pipe(imagemin())
+    .pipe(dest('app/images/dist'))
+    .pipe(
+			imagemin([
+
+			])
+		)
+
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
+}
+
+function buildcopy() {
+	return src([
+		'{app/js,app/css}/**/*.*',
+		'app/images/**/*.*',
+		'app/libs/**/*.*',
+		'!app/images/src/**/*',
+		'app/fonts/**/*'
+	], { base: 'app/' })
+	.pipe(dest('dist'))
+}
+
+async function buildhtml() {
+	let includes = new ssi('app/', 'dist/', '/**/*.html')
+	includes.compile()
+	del('dist/parts', { force: true })
+}
+
+async function cleandist() {
+	del('dist/**/*', { force: true })
+}
+
+
+function startwatch() {
+	watch(`app/styles/${preprocessor}/**/*`, { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch('app/images/src/**/*', { usePolling: true }, images)
+	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+}
+
+export { scripts, styles, images}
+export let assets = series(scripts, styles, images)
+export let build = series(cleandist, images, scripts, styles, buildcopy, buildhtml)
+
+export default series(scripts, styles, images, parallel(browsersync, startwatch))
